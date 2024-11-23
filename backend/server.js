0aa1b68a83650d3f459bbed8fc96571cc8c62df5
Bhavya Chavda <bhavyachavda55@gmail.com>
2024-11-23 14:09:58 +0530

    backend port added

--- backend/server.js
+++ backend/server.js
@@ -10,17 +10,11 @@ import orderRouter from "./routes/orderRoute.js";
 
 //app config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 //middleware
 app.use(express.json());
-app.use(cors(
-    {
-        origin: ["https://deploy-mern.vercel.app"],
-        methods: ["POST","GET"],
-        credentials: true
-    }
-))
+app.use(cors())
 
 config({
     path: "./.env",
