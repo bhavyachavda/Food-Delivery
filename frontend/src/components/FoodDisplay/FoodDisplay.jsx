c50d8ace7353f4a2a76bd11c488a2a8bef248e16
Bhavya Chavda <bhavyachavda55@gmail.com>
2024-12-03 19:39:41 +0530

    added loader

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -2,9 +2,24 @@ import React, { useContext, Suspense, lazy } from 'react'
 import "./FoodDisplay.css"
 import { StoreContext } from '../../context/StoreContext'
 // import FoodItem from '../FoodItem/FoodItem';
+import { RotatingLines } from "react-loader-spinner";
 
 // Lazy load FoodItem component
 const FoodItem = lazy(() => import("../FoodItem/FoodItem"));
+
+function Loader() {
+  return (
+    <div style={{textAlign: "center"}}>
+      <RotatingLines
+        strokeColor="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="96"
+        visible={true}
+      />
+    </div>
+  )
+}
 
 const FoodDisplay = (props) => {
 
@@ -15,6 +30,7 @@ const FoodDisplay = (props) => {
   return (
     <div className='food-display' id='food-display'>
         <h2>Top dishes near you</h2>
+        { food_list.length === 0 ? (<Loader/>) : <></>}
         <div className="food-display-list">
             <Suspense fallback={<div>Loading...</div>}>
             {food_list && food_list?.map((item,index)=>{
